9f85bffade8484afdef3cde2c2683357a7773281
Lasse Krogh Mammen <lkm@bookboon.com>
2013-12-08 18:42:59 +0000

    Take arguments for database file, pin number, sensitivity and verbosity

--- daemon/cpm.c
+++ daemon/cpm.c
@@ -29,6 +29,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
 #include <signal.h>
 #include <unistd.h>
 #include <sys/time.h>
@@ -36,134 +37,194 @@
 #include <wiringPi.h>
 #include <time.h>
 
-#define DATABASE_FILE	"power.sqlite3"
-#define PHYSICAL_PIN 	1
-#define SENSITIVITY		20
-
+static short int pin = 1;
+static short int sensitivity = 20;
 static short int keepRunning = 1;
+static short int verbose_flag;
 static volatile int lightcounter = 0;
 
 sqlite3 *initialize_db(char *dbfile) {
-	sqlite3 *pDb;
-	int rc;
-	char *zErrMsg = 0;
-	char *sql;
-	char *database = DATABASE_FILE;
-	
-	if (dbfile != NULL && strlen(dbfile) > 0) {
-		strcpy(database, dbfile);
-	}
-	
-	rc = sqlite3_open_v2(database, &pDb, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
-		
-	if (rc ) {
-		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(pDb));
-	   sqlite3_close(pDb);
-		exit(1);
-	}
-	
-	sql = "CREATE TABLE IF NOT EXISTS watthours ( \
+   sqlite3 *pDb;
+   int rc;
+   char *zErrMsg = 0;
+   char *sql;
+
+   rc = sqlite3_open_v2(dbfile, &pDb, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
+
+   if (rc) {
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(pDb));
+      sqlite3_close(pDb);
+      exit(1);
+   }
+
+   sql = "CREATE TABLE IF NOT EXISTS watthours ( \
 			id INTEGER PRIMARY KEY AUTOINCREMENT, \
 			datetime DATETIME DEFAULT CURRENT_TIMESTAMP, \
 			lightvalue INTEGER NOT NULL)";
-			
-	rc = sqlite3_exec(pDb, sql, NULL, 0, &zErrMsg);
-	
-	if (rc != SQLITE_OK ){
-		fprintf(stderr, "SQL error: %s\n", zErrMsg);
-		sqlite3_free(zErrMsg);
-		sqlite3_close(pDb);
-		exit(1);
-	}
-	    
-	return pDb;
+
+   rc = sqlite3_exec(pDb, sql, NULL, 0, &zErrMsg);
+
+   if (rc != SQLITE_OK) {
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+      sqlite3_close(pDb);
+      exit(1);
+   }
+
+   return pDb;
 }
 
 sqlite3_stmt *prepare_statement(sqlite3 *pDb) {
-	sqlite3_stmt *pStmt;
-	const char *sql = "INSERT INTO watthours (lightvalue) VALUES (:usage)";
-	
-	sqlite3_prepare_v2(pDb, sql, strlen(sql), &pStmt, NULL);
-	
-	return pStmt;				
+   sqlite3_stmt *pStmt;
+   const char *sql = "INSERT INTO watthours (lightvalue) VALUES (:usage)";
+
+   sqlite3_prepare_v2(pDb, sql, strlen(sql), &pStmt, NULL);
+
+   return pStmt;
 }
 
 void insert_data(int reading, sqlite3_stmt *pStmt) {
-	const int index = sqlite3_bind_parameter_index(pStmt, ":usage" );
-	sqlite3_bind_int(pStmt, index, reading);
-	
-	if (sqlite3_step(pStmt) != SQLITE_DONE) {
-		fprintf(stderr, "SQL error!\n");
-	}
-	sqlite3_reset(pStmt);
+   const int index = sqlite3_bind_parameter_index(pStmt, ":usage");
+   sqlite3_bind_int(pStmt, index, reading);
+
+   if (sqlite3_step(pStmt) != SQLITE_DONE) {
+      fprintf(stderr, "SQL error!\n");
+   }
+   sqlite3_reset(pStmt);
 }
 
 void intHandler() {
-    keepRunning = 0;
+   keepRunning = 0;
 }
 
 void interrupt_handler(void) {
-	++lightcounter;
-	/* When diode lights up to 2000 interrupt are fired, try to throttle a little */
-	usleep(100);
+   ++lightcounter;
+   /* When diode lights up to 2000 interrupt are fired, try to throttle a little */
+   usleep(100);
 }
 
 int main(int argc, char **argv) {
-	sqlite3 *pDb;
-	sqlite3_stmt *pStmt;
-	time_t timer;
-	char buffer[25];
-	struct tm* tm_info;
-	
-	signal(SIGINT, intHandler);
-	
-	pDb = initialize_db(NULL);
-	
-	/* set priority to maximum and die if fail */
-	if (piHiPri(99) != 0) {
-		fprintf(stderr, "Setting priorty of process failed\n");
-		exit(1);
-	}
-	
-	if (wiringPiSetup() == -1 ) {
-		fprintf(stderr, "WiringPI setup failed. You use sudo to run this program\n");
-		exit(1);
-	}
-	pinMode(PHYSICAL_PIN, INPUT) ;
-	pullUpDnControl(PHYSICAL_PIN, PUD_DOWN) ;
-	
-	if (wiringPiISR (PHYSICAL_PIN, INT_EDGE_RISING, &interrupt_handler) < 0) {
-		fprintf(stderr, "Interrupt failed.\n");
-		exit(1);
-	}
-	
-	pStmt = prepare_statement(pDb);
-	
-#ifdef DEBUG	
-	fprintf(stderr, "Starting main loop...\n");
-#endif
-	
-	while (keepRunning) {
-		if (lightcounter / SENSITIVITY) {
-#ifdef DEBUG
-		   time(&timer);
-	    	tm_info = localtime(&timer);
+   int c;
+   sqlite3 *pDb;
+   sqlite3_stmt *pStmt;
+   time_t timer;
+   char buffer[25];
+   char *database;
+   struct tm* tm_info;
 
-	    	strftime(buffer, 25, "%Y-%m-%d %H:%M:%S", tm_info);
-			fprintf(stderr, "\n%s Detected light level %d\n", buffer, lightcounter);
-#endif
-			insert_data(lightcounter, pStmt);
-			
-			/* Wait before resetting counter so all remaining "single blink" 
-			   interrupt are cleared */
-			delay(1000);
-			lightcounter = 0;
-		} else {
-			delay(1000);
-		}
-	}
-	
-	sqlite3_finalize(pStmt);
-	sqlite3_close(pDb);
-	return 0;
+   while (1) {
+      static struct option long_options[] ={
+         {"verbose", no_argument, &verbose_flag, 1},
+         {"help", no_argument, 0, 'h'},
+         {"database", required_argument, 0, 'd'},
+         {"pin", required_argument, 0, 'p'},
+         {"sensitivity", required_argument, 0, 's'},
+         {0, 0, 0, 0}
+      };
+
+      int option_index = 0;
+
+      c = getopt_long(argc, argv, "vhd:p:s:",
+              long_options, &option_index);
+
+      if (c == -1)
+         break;
+
+      switch (c) {
+         case 0:
+            if (long_options[option_index].flag != 0)
+               break;
+            printf("option %s", long_options[option_index].name);
+            if (optarg)
+               printf(" with arg %s", optarg);
+            printf("\n");
+            break;
+         case 'h':
+            printf("option -h with value ");
+            break;
+         case 'd':
+            if ((database = malloc(strlen(optarg)+1)) == NULL) {
+               fprintf(stderr, "Invalid value for argument for --database / -d \n");
+               exit(1);
+            }
+            strcpy(database, optarg);
+            break;
+         case 'p':
+            if (sscanf (optarg, "%i", &pin) != 1) {
+               fprintf(stderr, "Invalid value for argument for --pin / -p \n");
+               exit(1);
+            }
+            break;
+         case 's':
+            if (sscanf (optarg, "%i", &sensitivity) != 1) {
+               fprintf(stderr, "Invalid value for argument for --sensitivity / -s \n");
+               exit(1);
+            }
+            break;
+
+         default:
+            exit(1);
+      }
+   }
+
+   if (database == NULL) {
+      fprintf(stderr, "cpm: --database/-d is required\n");
+      exit(1);
+   }
+
+   signal(SIGINT, intHandler);
+
+   pDb = initialize_db(database);
+
+   /* set priority to maximum and die if fail */
+   if (piHiPri(99) != 0) {
+      fprintf(stderr, "Setting priorty of process failed\n");
+      exit(1);
+   }
+
+   if (wiringPiSetup() == -1) {
+      fprintf(stderr, "WiringPI setup failed. You use sudo to run this program\n");
+      exit(1);
+   }
+   pinMode(pin, INPUT);
+   pullUpDnControl(pin, PUD_DOWN);
+
+   if (wiringPiISR(pin, INT_EDGE_RISING, &interrupt_handler) < 0) {
+      fprintf(stderr, "Interrupt failed.\n");
+      exit(1);
+   }
+
+   pStmt = prepare_statement(pDb);
+
+   if (verbose_flag) {
+        fprintf(stderr, "Starting main loop...\n");
+   }
+
+   while (keepRunning) {
+      if (verbose_flag) {
+         fprintf(stderr, ".");
+      }
+      if (lightcounter / sensitivity) {
+         if (verbose_flag) {
+            time(&timer);
+            tm_info = localtime(&timer);
+
+            strftime(buffer, 25, "%Y-%m-%d %H:%M:%S", tm_info);
+            fprintf(stderr, "\n%s Detected light level %d\n", buffer, lightcounter);
+         }
+         insert_data(lightcounter, pStmt);
+
+         /* Wait before resetting counter so all remaining "single blink" 
+            interrupt are cleared */
+         delay(500);
+         lightcounter = 0;
+         
+      } else {
+         delay(500);
+      }
+   }
+
+   sqlite3_finalize(pStmt);
+   sqlite3_close(pDb);
+   return 0;
 }
